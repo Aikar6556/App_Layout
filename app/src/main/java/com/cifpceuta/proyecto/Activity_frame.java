776a9f63fa6136efdba8ea622cafe5040893bc66
tarde <266@cifpceuta.es>
2023-10-26 15:52:08 +0200

    Initial Commit

---
+++ app/src/main/java/com/cifpceuta/proyecto/Activity_frame.java
@@ -0,0 +1,63 @@
+package com.cifpceuta.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.VideoView;
+
+public class Activity_frame extends AppCompatActivity {
+
+
+    private VideoView videoView;
+
+    private ImageButton btnPlay, btnPause;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        btnPlay = findViewById(R.id.imageButtonPlay);
+        btnPause = findViewById(R.id.imageButtonPause);
+
+        videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.tortuga));
+
+    btnPlay.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            videoView.start();
+        }
+    });
+
+    btnPause.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            videoView.pause();
+        }
+    });
+
+
+
+    }
+
+    public void play (View v){
+
+        if (videoView.isPlaying()){
+            videoView.pause();
+        }else{
+            videoView.start();
+        }
+
+
+    }
+
+    public void metodo2 (View v){
+
+        videoView.pause();
+        btnPlay.setVisibility(View.VISIBLE);
+        btnPause.setVisibility(View.INVISIBLE);
+    }
+}
---
+++ app/src/main/java/com/cifpceuta/proyecto/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cifpceuta.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void botonLinear (View v){
+        Intent i = new Intent(this, ActivityLinear.class);
+        startActivity(i);
+    }
+
+    public void volver (View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void botonFrame (View v){
+        Intent i = new Intent(this, Activity_frame.class);
+        startActivity(i);
+    }
+}
