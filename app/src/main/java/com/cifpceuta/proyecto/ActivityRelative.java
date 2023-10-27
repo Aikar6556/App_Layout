511ff136c537370c2e4b5236eb4741c37077a899
tarde <266@cifpceuta.es>
2023-10-27 20:32:18 +0200

    Fase 2

--- app/src/main/java/com/cifpceuta/proyecto/ActivityRelative.java
+++ app/src/main/java/com/cifpceuta/proyecto/ActivityRelative.java
@@ -2,9 +2,13 @@ package com.cifpceuta.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ActivityRelative extends AppCompatActivity {
 
@@ -24,5 +28,22 @@ public class ActivityRelative extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listItem);
         array.setAdapter(adapter);
+
+        array.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(view.getContext(),array.getAdapter().getItem(i).toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+
+    public void inicio (View view){
+
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
     }
 }
